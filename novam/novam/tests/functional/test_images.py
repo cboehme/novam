8b1de57491fda8a2e41927f76d843b92b5c5e1ff
Christoph Böhme <christoph@b3e.net>
2009-11-12 20:21:51 +0000

    - Removed unused files - Added GPL

--- novam/novam/tests/functional/test_images.py
+++
@@ -1,8 +0,0 @@
-from novam.tests import *
-
-class TestImagesController(TestController):
-
-    def test_index(self):
-		pass
-        #response = self.app.get(url(controller='images', action='index'))
-        # Test response...

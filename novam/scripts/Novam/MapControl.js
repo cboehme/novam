4ac0a9b9f2e257eb0e94aba573deb8a43ba2c2f2
Christoph Böhme <christoph@b3e.net>
2011-11-14 20:46:03 +0000

    Fixed xapi uris

--- novam/scripts/Novam/MapControl.js
+++ novam/scripts/Novam/MapControl.js
@@ -233,13 +233,13 @@ Novam.MapControl = Class.create({
 			this.map_status.show();
 
 			OpenLayers.Request.GET({
-				url: "node[bbox="+bounds.toBBOX()+"][highway=bus_stop]",
+				url: "/xapi/node[bbox="+bounds.toBBOX()+"][highway=bus_stop]",
 				scope: this,
 				success: this._get_request_handler()
 			});
 
 			OpenLayers.Request.GET({
-				url: "node[bbox="+bounds.toBBOX()+"][naptan:AtcoCode]",
+				url: "/xapi/node[bbox="+bounds.toBBOX()+"][naptan:AtcoCode]",
 				scope: this,
 				success: this._get_request_handler()
 			});
